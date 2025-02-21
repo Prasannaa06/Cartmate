170b5ec082aa77f44425ec3da6509ed1d80d70ec
Harsha-Hemanth <158434803+Harsha-Hemanth@users.noreply.github.com>
2025-02-22 01:57:19 +0530

    equalized loading image size equal to loaded image size

--- frontend/src/components/VerticalCard.jsx
+++ frontend/src/components/VerticalCard.jsx
@@ -20,8 +20,8 @@ const VerticalCard = ({loading, data = []}) => {
          loading ? (
              loadingList.map((product, index)=>{
                  return(
-                     <div className='w-full min-w-[280px]  md:min-w-[320px] max-w-[280px] md:max-w-[320px]  bg-white rounded-sm shadow' key={index}>
-                         <div className='bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center animate-pulse'>
+                     <div className='w-full min-w-[220px]  md:min-w-[250px] max-w-[220px] md:max-w-[250px]  bg-white rounded-sm shadow' key={index}>
+                         <div className='bg-slate-200 h-48 p-4 min-w-[220px] md:min-w-[250px] flex justify-center items-center animate-pulse'>
                          </div>
                          <div className='p-4 grid gap-3'>
                              <h2 className='font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black p-1 py-2 animate-pulse rounded-full bg-slate-200'></h2>
